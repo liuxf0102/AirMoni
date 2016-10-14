a29215fb76918307bc54e6fe440ce8f76df29867
xianfliu <xianfliu@XIANFLIU-CN.cn.oracle.com>
2016-10-14 09:59:41 +0800

    origin

--- src/js/viewModels/graphics.js
+++ src/js/viewModels/graphics.js
@@ -8,12 +8,24 @@ function(oj, ko, $)
         self.orientationValue = ko.observable('vertical');      
         self.lineTypeValue = ko.observable('curved');
         
+        
+        var pm25Series = new Array();
+        
+        pm25Series.push(174);
+        pm25Series.push(62);
+        pm25Series.push(70);
+        pm25Series.push(76);
+        pm25Series.push(66);
+        pm25Series.push(80);
+        
+        
         /* chart data */
-        var lineSeries = [{name : "温度", items : [74, 62, 70, 76, 66,80]} ];
-    
+        //var lineSeries = [{name : "温度", items : [74, 62, 70, 76, 66,80]} ];
+        var lineSeries = [{name : "温度"} ];
+         lineSeries[0].items=pm25Series;
         var lineGroups = ["9.21", "9.22", "9.23", "9.24", "9.25","9.26"];
    
-        
+        console.log(lineSeries[0].items);
         this.lineSeriesValue = ko.observableArray(lineSeries);
         this.lineGroupsValue = ko.observableArray(lineGroups);
         
